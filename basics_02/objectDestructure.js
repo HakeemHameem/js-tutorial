4bba9aa204853b17261dd3350cf589ea5988b1da
HakeemHameem <123749814+HakeemHameem@users.noreply.github.com>
2023-11-16 18:30:41 +0000

    object destructuring ki kahani

---
+++ basics_02/objectDestructure.js
@@ -0,0 +1,9 @@
+const halal={
+    name:"Hameem",
+    usn:"usn",
+    branch:"ise"
+}
+
+const{branch:aanch }=halal     // this is known as the object destructuring i.e we can change the value of a key and give it to another name                
+
+console.log(aanch)
