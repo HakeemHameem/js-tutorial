13dd27682d5023e2374eb53f27c1ae83643f98a8
Hameem <123749814+HakeemHameem@users.noreply.github.com>
2023-11-29 11:26:45 +0000

    promisses

--- Advanced_script/PromisePrac.js
+++ Advanced_script/PromisePrac.js
@@ -19,5 +19,20 @@
 // })
 
 const Prom=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=false
+        if (!error) {
+            
+            console.log("the settimeout is activated")
+            resolve({name:"Hameem",usn:"1MJ21IS039",branch:"ise"})
+        }
+        else{
+            console.log("error")
+        }
+
+    },1000)
+})
+Prom.then(function(usser){
+    console.log(usser)   
     
 })
